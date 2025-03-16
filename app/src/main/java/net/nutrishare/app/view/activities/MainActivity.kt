f86588cc07f2747fed2919ee99f63e58a95c2413
Yuvalya101 <yuvallavi249@gmail.com>
2025-03-16 14:07:34 +0200

    setup bottom navigation with three fragments (feed,favourite,profile) design

--- app/src/main/java/net/nutrishare/app/view/activities/MainActivity.kt
+++ app/src/main/java/net/nutrishare/app/view/activities/MainActivity.kt
@@ -2,11 +2,48 @@ package net.nutrishare.app.view.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import net.nutrishare.app.R
+import net.nutrishare.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpToolbar()
+        setUpNavController()
+    }
+
+    private fun setUpToolbar(){
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.apply {
+            setTitleTextColor(ContextCompat.getColor(context,R.color.white))
+        }
+    }
+
+    private fun setUpNavController(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_home) as NavHostFragment
+
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
+        binding.bottomNavigationView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
